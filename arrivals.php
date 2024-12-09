b37ccad01dfd19747939e81eb3b1243c7b90c9e1
Test <comiksansqwerty@gmail.com>
2024-12-09 13:33:22 +0700

    daa

--- arrivals.php
+++ arrivals.php
@@ -1,6 +1,6 @@
 <?php
 $pdo = require $_SERVER['DOCUMENT_ROOT'] . '/zadanie/db.php';
-$prodq = $pdo->prepare('select * from products');
+$prodq = $pdo->prepare('select uchet_tovarov.amount, uchet_tovarov.date, products.name as product_name  from uchet_tovarov left join products on uchet_tovarov.product_id = products.id ');
 $prodq->execute();
 $prod= $prodq->fetchAll(PDO::FETCH_ASSOC);
 ?>
@@ -13,14 +13,14 @@ $prod= $prodq->fetchAll(PDO::FETCH_ASSOC);
         <link href="css/style.css" rel="stylesheet">
     </head>
     <body>
-     <a href="/zadanie/arrivals.php">Arrivals</a>
      <table>
-        <tr><td>Name</td><td>Cost</td><td>Article</td><td><a href="/zadanie/create.php">Add new product</a></td><td><a href="/zadanie/postup.php">Add arrive of product</a></td></tr>
+         <a href="/zadanie/index.php">Home</a>
+        <tr><td>Name</td><td>Amount</td><td>Date</td>></tr>
         <?foreach($prod as $pr):?>
            <tr>
-           <td><?=$pr['name']?></td>
-           <td><?=$pr['how_much']?></td>
-           <td><?=$pr['id']?></td>
+           <td><?=$pr['product_name']?></td>
+           <td><?=$pr['amount']?></td>
+           <td><?=$pr['date']?></td>
            </tr> 
         <?endforeach?>
      </table> 
