6823aef6661049f055811995a7828ae347ca11b0
Bratok2202 <piskabobra12sm@gmail.com>
2024-12-09 13:06:49 +0700

    new

---
+++ postup.php
@@ -0,0 +1,34 @@
+<?php
+$pdo = require $_SERVER['DOCUMENT_ROOT'] .  '/zadanie/db.php';
+
+$products = $pdo->query('select * from products');
+$products = $products->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>postuplenie tovara</h1>
+    <form action="/zadanie/actions/postup.php" method="post">
+        <label for="date">date</label>
+        <input type="text" name="date" placeholder="y-m-d    exemple: 2024-12-12" id="date"><br>
+        <label for="prod">name of product</label><select name="product" id="prod">
+            <? foreach ($products as $prod): ?>
+                <option value="<?= $prod['id'] ?>"><?= $prod['name'] ?></option>
+            <? endforeach ?>
+        </select>
+        <br>
+        <label for="amount">amount of product</label>
+        <input type="number" id="amount" name="amount">
+        <input type="submit">
+    </form>
+</body>
+
+</html>
