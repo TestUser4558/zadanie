d5971942a96407ebc1918cc0a208521372e0e023
TestUser4558 <comiksansqwerty@gmail.com>
2024-12-11 10:40:26 +0700

    Add files via upload

---
+++ uchet_tovarov.sql
@@ -0,0 +1,78 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: database:3306
+-- Generation Time: Dec 11, 2024 at 03:39 AM
+-- Server version: 8.4.2
+-- PHP Version: 8.2.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `uchet`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `uchet_tovarov`
+--
+
+CREATE TABLE `uchet_tovarov` (
+  `id` int NOT NULL,
+  `date` date NOT NULL,
+  `product_id` int NOT NULL,
+  `amount` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `uchet_tovarov`
+--
+
+INSERT INTO `uchet_tovarov` (`id`, `date`, `product_id`, `amount`) VALUES
+(1, '2024-12-12', 1, 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `uchet_tovarov`
+--
+ALTER TABLE `uchet_tovarov`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `uchet_tovarov`
+--
+ALTER TABLE `uchet_tovarov`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `uchet_tovarov`
+--
+ALTER TABLE `uchet_tovarov`
+  ADD CONSTRAINT `uchet_tovarov_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
